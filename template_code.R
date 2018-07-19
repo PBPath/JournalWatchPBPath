2c67a997f59ecd722fc299fb7c7dd94dfd9cc652
sbalci <drserdarbalci@gmail.com>
2018-07-19 12:07:51 +0300

    updated on 2018-07-19 12:07:51

--- template_code.R
+++ template_code.R
@@ -1,18 +1,48 @@
-PMID_25783680 <- RefManageR::ReadPubMed("25783680", database = "PubMed")
+```{r include=FALSE}
+PubMedString <- "PubMed: https://www.ncbi.nlm.nih.gov/pubmed/?term="
 
-cit_25783680 <- paste0(PMID_25783680$title, " ", PMID_25783680$journal, " ", "PMID: https://www.ncbi.nlm.nih.gov/pubmed/?term=", PMID_25783680$eprint, " ", "doi: https://doi.org/", PMID_25783680$doi)
+doiString <- "doi: https://doi.org/"
 
-dimensionBadge <- paste0(
-    "<script async='' charset='utf-8' src='https://badge.dimensions.ai/badge.js'></script>
-    <span class='__dimensions_badge_embed__' data-doi='",
-    PMID_25783680$doi,
-    "' data-style='small_circle'></span>"
+dimensionString1 <- "<script async='' charset='utf-8' src='https://badge.dimensions.ai/badge.js'></script> <span class='__dimensions_badge_embed__' data-doi='"
+
+dimensionString2 <- "' data-style='small_circle' data-hide-zero-citations='true'></span>"
+
+altmetricString1 <-"<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script> <span class='altmetric-embed' data-badge-popover='right' data-badge-type='donut' data-doi='"
+
+altmetricString2 <- "' data-hide-no-mentions='true'></span>"
+```
+
+
+```{r include=FALSE}
+PMID_29684363 <- RefManageR::ReadPubMed("29684363",
+                                        database = "PubMed")
+
+PubMed_29684363 <- paste0(PubMedString,
+                          PMID_29684363$eprint
 )
 
-
-altmetricBadge <- paste0(
-    "<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>
-<span class='altmetric-embed' data-badge-popover='right' data-badge-type='donut' data-doi='",
-    PMID_25783680$doi,
-    "'></span>"
+doi_29684363 <- paste0(doiString,
+                       PMID_29684363$doi
 )
+
+dimensionBadge_29684363 <- paste0(dimensionString1,
+                                  PMID_29684363$doi,
+                                  dimensionString2
+)
+
+altmetricBadge_29684363 <- paste0(altmetricString1,
+                                  PMID_29684363$doi,
+                                  altmetricString2
+)
+```
+
+- Immunolabeling of Cleared Human Pancreata Provides Insights into Three-Dimensional Pancreatic Anatomy and Pathology.
+
+`r PubMed_29684363`
+
+`r doi_29684363`
+
+`r dimensionBadge_29684363`
+
+`r altmetricBadge_29684363`
+
